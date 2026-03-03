9af392c3e176191a771688f13e72713824bd6a59
peoplesmilitia <achhachhiya1@nait.ca>
2026-03-03 13:13:00 -0700

    made changes working on lesson 13

---
+++ lesson-12-starter/src/components/Tester.jsx
@@ -0,0 +1,27 @@
+import { useState} from 'react';
+export default function Tester() {
+    const [searchTerm, setSearchTerm] = useState("")
+    return (
+    <>
+        <input
+            type="text"
+            value={searchTerm}
+            placeholder = "Search resources..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <p className="alert alert-info mt-4">
+            {searchTerm}
+        </p>
+    </>
+    );
+}
+
+
+/*export default function Tester() {
+    const [isOn, setIsOn] = useState(true);
+    return(
+        <button onClick={() => setIsOn(!isON)}>
+            {isOn? "ON" : "OFF"}
+        </button>
+    );
+}*/
